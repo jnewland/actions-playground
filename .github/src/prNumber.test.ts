13b15c2cbe7c5307eb0958627a07ac4e5bd6df78
Jesse Newland <jesse@jnewland.com>
2023-05-13 16:37:01 +0000

    Test this too

--- .github/src/prNumber.test.ts
+++ .github/src/prNumber.test.ts
@@ -1,8 +1,22 @@
 import { GitHubScriptArguments } from "./types";
 import { prNumber } from "./prNumber";
-import { Octokit } from "./__mocks__/github";
 import { Context } from "@actions/github/lib/context";
 import * as core from "./__mocks__/core";
+import { Octokit } from "@octokit/rest";
+
+jest.mock("@octokit/rest", () => {
+    const Octokit = class MockOctokit {
+        rest = {
+            pulls: {
+                list: jest.fn().mockReturnValue({
+                    data: [{ number: 5678 }],
+                }),
+            },
+        };
+    };
+
+    return { Octokit };
+});
 
 describe("prNumber", () => {
     it("should set the prNumber from the context", async () => {
@@ -36,7 +50,6 @@ describe("prNumber", () => {
         };
 
         const args: GitHubScriptArguments = {
-            github: new Octokit(),
             context: context,
             core: core,
         };
@@ -44,5 +57,44 @@ describe("prNumber", () => {
         const stringNumber = await prNumber(args);
 
         expect(stringNumber).toEqual("1234");
+    });
+
+    it("or from the API if not present", async () => {
+        const context: Context = {
+            payload: {
+                pull_request: undefined,
+            },
+            issue: {
+                number: 0,
+                owner: "",
+                repo: "",
+            },
+            repo: {
+                owner: "",
+                repo: "",
+            },
+            eventName: "",
+            sha: "",
+            ref: "",
+            workflow: "",
+            action: "",
+            actor: "",
+            job: "",
+            runNumber: 0,
+            runId: 0,
+            apiUrl: "",
+            serverUrl: "",
+            graphqlUrl: "",
+        };
+
+        const args: GitHubScriptArguments = {
+            context: context,
+            github: new Octokit(),
+            core: core,
+        };
+
+        const stringNumber = await prNumber(args);
+
+        expect(stringNumber).toEqual("5678");
     });
 });
