aefab67ede2904cc9e0c553da20d66407905a068
Jesse Newland <jesse@jnewland.com>
2023-05-13 15:24:35 +0000

    cleanup

--- .github/src/__mocks__/github.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Octokit = void 0;
-exports.Octokit = jest.fn().mockImplementation(function () {
-    return {
-        // Mock the properties and methods of the Octokit object
-    };
-});
